3178786aba8bae2982807667d440cb2fe9abc1e4
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-21 17:50:59 +0400

    with prisma seeds

--- prisma/seeds.ts
+++ prisma/seeds.ts
@@ -1,19 +1,29 @@
+import {PrismaClient} from "@prisma/client";
 
-
-
+const prisma = new PrismaClient()
 
 
 const initialPosts = [
     {
         title: "some title 1",
-        body: "some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 "
+        body: "some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1"
     },
     {
         title: "some title 2",
-        body: "some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 "
+        body: "some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2"
     },
     {
         title: "some title 3",
-        body: "some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 "
+        body: "some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3"
     },
 ]
+
+const seeds = async () => {
+    await prisma.post.deleteMany()
+
+    for (const post of initialPosts) {
+        await prisma.post.create({data:{title:post.title, body:post.body}})
+    }
+}
+
+seeds()
