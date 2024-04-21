c181c1cf1c6486ada9dd95f3f63bf5fb20952695
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-21 21:07:39 +0400

    with correction 2 of type Post (define separately)

--- services/posts.ts
+++ services/posts.ts
@@ -1,12 +1,19 @@
 import {prisma} from "@/lib/prisma";
+import {Prisma, PrismaPromise} from "@prisma/client";
+import Prisma__PostClient = Prisma.Prisma__PostClient;
+import {DefaultArgs} from "@prisma/client/runtime/binary";
 
-export function getPostById(id: string) {
+export function getPostById(id: string): Prisma__PostClient<{
+    id: string;
+    title: string;
+    body: string;
+} | null, null, DefaultArgs> {
     return prisma.post.findUnique({
         where: {id}
     })
 
 }
 
-export function getAllPosts() {
-    return prisma.post.findMany( )
+export function getAllPosts(): PrismaPromise<{ id: string, title: string, body: string }[]> {
+    return prisma.post.findMany()
 }
