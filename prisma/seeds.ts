9ddb7d0a41e983d0fea8cc85cbb631a249ef728e
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-21 17:27:39 +0400

    with InitialPosts

---
+++ prisma/seeds.ts
@@ -0,0 +1,19 @@
+
+
+
+
+
+const initialPosts = [
+    {
+        title: "some title 1",
+        body: "some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 some body 1 "
+    },
+    {
+        title: "some title 2",
+        body: "some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 some body 2 "
+    },
+    {
+        title: "some title 3",
+        body: "some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 some body 3 "
+    },
+]
